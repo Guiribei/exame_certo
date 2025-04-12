2d8521e13dfd00771152cc47d7fc6846af8ecee8
Guilherme Ribeiro <85483589+Guiribei@users.noreply.github.com>
2025-04-12 15:22:25 -0300

    Update settings.py

--- examecerto/settings.py
+++ examecerto/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jn$k(f!qc5(d_8boi$42z!pa6d-v#g^2u^g2b34577%q%x3^4z'
+SECRET_KEY = 'secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
